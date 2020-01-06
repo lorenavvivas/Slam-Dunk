979b293955c3cb2561c5ec57ddcc732a41b55d5f
lorena <vivas.v.lorena@gmail.com>
2020-01-06 20:12:22 -0300

    Ejercicio 3

---
+++ ejercicio_3.py
@@ -0,0 +1,17 @@
+import csv
+with open('estadisticas_partidos.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    print('-' * 65)
+    print('|{:^15}|{:^15}|{:^15}|{:^15}|'.format('Team 1', 'S', 'Team 2', 'S'))
+    for i, v in enumerate(reader):
+        try:
+            score_1, score_2 = v['Score'].split('-')
+        except ValueError as e:
+            score_1 = 'Unknown'
+            score_2 = 'Unknown'
+
+        if i == 0:
+            print('-' * 65)
+        print('|{:^15}|{:^15}|{:^15}|{:^15}|'.format(v['Team 1'], score_1, v['Team 2'], score_2))
+    print('-' * 65)
+
